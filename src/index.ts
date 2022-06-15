535152d1ae080aedcce955622f58f919aa3542d9
angelxescomx08 <ah@xdevelop.mx>
2022-06-14 21:55:10 -0500

    primer observable

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,27 @@
+import { Observable } from "rxjs";
 
+/**
+ * Observable
+ * crea un observable
+ * @params subscriber {funcion}
+ */
+const obs$ = new Observable<string>(subscriber =>{
+    subscriber.next('Hola');
+    subscriber.next('mundo');
 
-console.log('Hola Mundo! ');
+    subscriber.next('Hola');
+    subscriber.next('mundo');
 
+    subscriber.complete();
 
+    subscriber.next('Hola');
+    subscriber.next('mundo');
+});
 
-
-
+/**
+ * subscribe
+ * @params next {funcion}
+ */
+obs$.subscribe(console.log);
 
 
