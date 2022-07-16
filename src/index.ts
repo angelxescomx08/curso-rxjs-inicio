1c796d656bce0c75c6c2f36563f60309a560a383
angelxescomx08 <ah@xdevelop.mx>
2022-07-15 20:34:27 -0500

    merge all

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,26 @@
-import {ajax} from 'rxjs/ajax';
+import { debounceTime, fromEvent, map, mergeAll } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
+const body = document.querySelector('body');
+const textInput = document.createElement('input');
+const ol = document.createElement('ol');
 
-const url = 'https://httpbin.org/delay/1';
+body.append(textInput,ol);
 
-ajax.post(url,{
-    id: 1,
-    nombre: 'Ángel'
-},{
-    'mi-token': 'ABC123'
-}).subscribe(console.log);
+const input$ = fromEvent<KeyboardEvent>(textInput,'keyup');
 
-ajax.put(url,{
-    id: 1,
-    nombre: 'Ángel'
-},{
-    'mi-token': 'ABC123'
-}).subscribe(console.log);
 
-ajax.delete(url,{
-    'mi-token': 'ABC123'
-}).subscribe(console.log);
-
-ajax({
-    url,
-    method: 'POST',
-    headers:{
-        'mi-token': 'ABC123'
-    },
-    body: {
-        id: 1,
-        nombre: 'Ángel'
-    }
-}).subscribe(console.log)
+/**
+ * mergeAll - ******Se subscribe a un observable emitido por el operador anterior, aplana el observable*******
+ * Convierte un Observable de orden superior en un Observable de primer orden que entrega simultáneamente 
+ * todos los valores que se emiten en los Observables internos.
+ */
+input$.pipe(
+    debounceTime(500),
+    map(event=> event.target as HTMLInputElement),
+    map(target=>target.value),
+    map((text)=>ajax.getJSON(`https://api.github.com/search/users?q=${text}`)),
+    mergeAll(),
+    map((res:any)=>res.items)
+).subscribe(res=>{
+    console.log(res);
+})
