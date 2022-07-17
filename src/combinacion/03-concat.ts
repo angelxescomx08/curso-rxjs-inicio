ba597c7300dfda4e44bd53a891de4afa3541bdd1
angelxescomx08 <ah@xdevelop.mx>
2022-07-17 12:16:00 -0500

    concat y lab

---
+++ src/combinacion/03-concat.ts
@@ -0,0 +1,14 @@
+import { concat, interval, of } from 'rxjs';
+import { take } from 'rxjs/operators';
+
+const interval$ = interval(1000);
+
+/**
+ * concat - función que permite concatenar observables
+ */
+concat(
+    interval$.pipe(take(3)),
+    interval$.pipe(take(2)),
+    [1,2,3,4],
+    of(1)
+).subscribe(console.log);
